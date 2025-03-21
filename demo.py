81b1eebd6c9b0e15641b1b0488caae7b03c46a84
Hitman-coder <131787968+Hitman-coder@users.noreply.github.com>
2025-03-21 08:30:27 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,49 @@
+# write a program to remove armstrong number
+
+# num=int(input("enter the number")) 
+# sum=0
+# temp=num
+# order=(len(str(num)))
+# while temp>0:
+#     digit=temp%10
+#     sum+=digit**order
+#     temp//=10
+# if num==sum:
+#     print(num,"is an armstrong number")
+# else:
+#     print(num,"is not an armstrong number")
+
+
+# write a program to perferct number
+
+# num=int(input("enter the number"))
+# sum=0
+# i=1
+# while i<num:
+#     if num%i==0:
+#         sum+=i
+#     i+=1
+# if num==sum:
+#     print(num,"is a perfect number")
+# else:
+#     print("it is imperfect number")
+
+
+# write a program to remove disarium  number 
+
+# num=int(input("enter the number"))
+# sum=0
+# temp=num
+# order=(len(str(num)))
+# while temp>0:
+#     digit=temp%10
+#     sum+=digit**order
+#     temp//=10
+#     order-=1
+# if num==sum:
+#     print(num,"is an disarium number")
+# else:
+#     print(num,"is not an disarium number")
+
+     
+
